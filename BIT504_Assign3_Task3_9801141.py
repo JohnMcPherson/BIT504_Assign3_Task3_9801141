6912122cd35c108a88516e709d819e9d0c1a047b
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-01-31 15:27:11 +1300

    First commit to Python solution to Assignment 3 Task 3 Well on the way!

--- BIT504_Assign3_Task3_9801141.py
+++ BIT504_Assign3_Task3_9801141.py
@@ -14,7 +14,11 @@ print("3. Display palindromes up to 1,000")
 # I assume this program does not have to do any checking beyond wha the Java does
 
 option = int(input("Enter an option: "))
-
+# Display the chosen option (on the same line as the prompt)
+# Note that Python displays >? (unlike Java). I have assumed we are happy to
+# keep that, as this is a Python command line program, and it makes sense to run
+# with the conventions of the framework
+print(option)
 # Check validity of option
 # Use chained comparison to
 # no parentheses required
@@ -25,20 +29,18 @@ if 1 <= option <= 3:
     match option:
         case 1:
             # confirm the path the user has chosen
-            # Note the ADDITIONAL leading "\n"; to get a new line directly after an input statement
-            # Otherwise, we would get one less line than the original Java program
-            print("\n\n**** Area of a square ****")
+            print("\n**** Area of a square ****")
 
             # Get the width of the square
             # We need both the string and the integer representations because
             # (unlike Java) Python does not automatically convert integer to string
             # when concatenating
-
             strWidth = input("Enter width of square (cm): ")
             # Again, in doing the conversion to integer we are not checking that the
             # conversion will succeed. The program will crash if it doesn't
             intWidth = int(strWidth)
-            # Note the leading "\n"; to get a new line directly after an input statement
-            print("\nThe area of a square of " + strWidth + "cm = " + "areaSquare(width)" + "cm squared");
+            # Note the leading "\n"; to get a new line directly after the input statement
+            # Note the use of both strWidth and intWidth
+            print("\nThe area of a square of " + strWidth + "cm = " + "areaSquare(width)" + "cm squared")
 
 
